ec2d569ec0178680ddb63d6642114f31e446e139
James Martinez <airtorque1@gmail.com>
2020-06-03 22:31:23 -0400

    fixed syntaz error in mongodb localhost thing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/3001/herokulesson';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/3001:herokulesson';
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
 // Error / success
